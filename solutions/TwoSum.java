f81b7c120e92e6401b5d645283e194249869d407
GokceSoylu <gokcesoylu24@gmail.com>
2026-02-26 14:41:05 +0300

    try commit

---
+++ solutions/TwoSum.java
@@ -0,0 +1,14 @@
+package solutions;
+
+import java.util.*;
+
+public class TwoSum {
+    public static void main(String[] arg) {
+        int a;
+        int b;
+        Scanner in = new Scanner(System.in);
+        a = in.nextInt();
+        b = in.nextInt();
+        System.out.println(a + b);
+    }
+}
